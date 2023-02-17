d45148e50267bafb92b8250695d673218e8b98ab
silvhua <silvhua@gmail.com>
2023-02-17 08:50:35 -0800

    copied from main folder of custom python scripts

---
+++ src/silvhua.py
@@ -0,0 +1,85 @@
+# 2023-02-17 for online social media project
+import pandas as pd
+ 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+from datetime import datetime
+
+# 2022-10-27 17:02 Update the sampling function to avoid loading entire dataframe.
+def load_csv(filename,filepath,column1_as_index=False,truncate=None, usecols=None, sep=','):
+    """
+    Load a csv file as a dataframe using specified file path copied from windows file explorer.
+    Back slashes in file path will be converted to forward slashes.
+    Arguments:
+    - filepath (raw string): Use the format r'<path>'.
+    - filename (string).
+    - colum1_as_index (bool): If true, take the first column as the index. 
+        Useful when importing CSV files from previously exported dataframes.
+
+    Returns: dataframe object.
+    """
+    filename = f'{filepath}/'.replace('\\','/')+filename
+    df = pd.read_csv(filename, usecols=usecols, sep=sep)
+    if column1_as_index==True:
+        df.set_index(df.columns[0], inplace=True)
+        df.index.name = None
+    print('Dataframe shape: ',df.shape)
+    print('Time completed:', datetime.now())
+
+    if truncate:
+        return df.sample(n=truncate,random_state=0)
+    else:
+        return df
+
+def save_csv(df,filename,path=None,append_version=False):
+    """
+    Export dataframe to CSV.
+    Parameters:
+    - df: Dataframe variable name.
+    - filename: Root of the filename.
+    - filepath (raw string): Use the format r'<path>'. If None, file is saved in same director.
+    - append_version (bool): If true, append date and time to end of filename.
+    """
+    if path:
+        path = f'{path}/'.replace('\\','/')
+    if append_version == True:
+        filename+=datetime.now().strftime('%Y-%m-%d_%H%M')
+    df.to_csv(path+filename+'.csv')
+    print('File saved: ',path+filename+'.csv')
+    print('Time completed:', datetime.now())
+
+
+def savepickle(model,filename, ext='sav', path=None,append_version=False):
+    """
+    Export object as a pickle.
+    Parameters:
+    - model: Model variable name.
+    - filename: Root of the filename.
+    - extension: Extension to append (do not include dot as it will be added)
+    - filepath (raw string): Use the format r'<path>'. If None, file is saved in same director.
+    - append_version (bool): If true, append date and time to end of filename.
+    """
+    if path:
+        path = f'{path}/'.replace('\\','/')
+    if append_version == True:
+        filename+=datetime.now().strftime('%Y-%m-%d_%H%M')
+    with open (path+filename+'.'+ext, 'wb') as fh:
+        pickle.dump(model, fh)
+    print('File saved: ',path+filename+'.'+ext)
+    print('Time completed:', datetime.now())
+
+def loadpickle(filename,filepath):
+    """
+    Load a pickled model using specified file path copied from windows file explorer.
+    Back slashes in file path will be converted to forward slashes.
+    Arguments:
+    - filepath (raw string): Use the format r'<path>'.
+    - filename (string).
+    
+    Returns saved object.
+    """
+    filename = f'{filepath}/'.replace('\\','/')+filename
+    loaded_model = pickle.load(open(filename, 'rb'))
+    print('Time completed:', datetime.now())
+    return loaded_model
