a53c060ac6c1ac6898e2dce30d21ef8fe939921e
silvhua <silvhua@gmail.com>
2023-02-23 03:03:46 -0800

    clean up script

--- src/processing.py
+++ src/processing.py
@@ -100,69 +100,6 @@ def preprocess_post_text(doc):
     except: # In case value is nan
         return 'zzzEmpty'
     
-# def preprocess_post_text(doc):
-#     """
-#     Prepare data from text documents for NLP:
-#     - Convert to lowercase
-#     - Remove formatting
-#     - Remove all the special characters
-#     - Remove all single characters
-#     - Substitute multiple spaces with single space
-
-#     Parameters:
-#     doc (string): Document.
-
-#     Returns: Processed doc.
-#     """
-#     wnl = WordNetLemmatizer()
-#     # Remove apostrophes before tokenization to preserve contractions like "should've"
-#     doc = re.sub(r"(\b\w+)'(\w+\b)", r'\1\2', doc)
-
-#     # Split text into single words (also gets rid of extra white spaces)
-#     words = word_tokenize(doc)
-
-#     # Remove text formatting
-#     words = [unicodedata.normalize('NFKD', word) for word in words]
-    
-#     # Convert to lower case
-#     words = [word.lower() for word in words]
-
-#     # Remove stop words
-#     stop_words = set(stopwords.words('english'))
-#     words = [word for word in words if not word in stop_words]
-
-#     # Lemmatize words (must be done after conversion to lower case)
-#     words = [wnl.lemmatize(word, pos='v') for word in words]
-#     words = [wnl.lemmatize(word, pos='n') for word in words]
-    
-#     # join words back together as a string
-#     words = ''.join([word+' ' for word in words])
-
-#     # Join @ and # to the subsequent word to retain handles and hashtags
-#     words = re.sub(r'@ \w+', 'zzzHandle', words)
-#     words = re.sub(r'# (\w+)', r'zzzHashtag\1', words)
-
-#     # Remove any URLs 
-#     words = re.sub(r'\w*\.+\w*', '', words) # Remove periods in middle of word
-#     words = re.sub(r'\w*/+\w*', '', words) # remove forward slash in middle of word
-#     words = re.sub(r'\w*/+\w*', '', words)
-
-#     # Replace hypthens with spaces
-#     words = re.sub(r'(\w+)-(\w+)*', r'\1 \2', words)
-#     words = re.sub(r'(\w+)-(\w+)*', r'\1 \2', words)
-
-#     # Remove numbers
-#     words = re.sub(r'\d:\d\d[\-a-zA-Z]*','zzzTime', words) # Time of day
-#     words = re.sub(r'\b\d+\b', 'zzzNumber', words)
-#     words = re.sub(r'\b\d+\w+\b', 'zzzNumber', words) #Number with letters
-
-#     # remove special characters
-#     non_hashtag_punctuation = ''.join([char for char in string.punctuation if char not in '#@'])
-#     words = ''.join([char for char in words if char not in non_hashtag_punctuation])
-
-#     return words
-
-
 def post_preprocessing(input_df, text_column='caption', n_top_to_print=10,
     filename=None,
     path=r'C:\Users\silvh\OneDrive\lighthouse\portfolio-projects\online-PT-social-media-NLP\data\interim', **kwargs):
@@ -247,5 +184,4 @@ def tfidf_transform(count_vector):
         vectorizer.fit_transform(count_vector).toarray(), 
         columns=vectorizer.get_feature_names_out()
         )
-    print('TF-IDF done')
     return tfidf
