d082c3196e574b8d3d4d6a54e08042d6d19e3331
silvhua <silvhua@gmail.com>
2023-02-25 12:13:52 -0800

    changed test.py to About.py with info about me and links

---
+++ src/pages/2_About.py
@@ -0,0 +1,41 @@
+import streamlit as st
+# from processing import *
+# from silvhua import *
+# import pandas as pd
+
+# import string
+# import re
+# import json
+# import numpy as np
+# from nltk.tokenize import word_tokenize
+# import numpy as np
+# from nltk.stem import WordNetLemmatizer
+# import pandas as pd
+# import unicodedata
+# from nltk.corpus import stopwords
+# from sklearn.feature_extraction.text import CountVectorizer
+# from datetime import datetime
+# import nltk
+# nltk.download('punkt')
+# nltk.download('stopwords')
+
+# st.write('This is a page for Silvia to test and debug code')
+
+# docs = [   
+#     "The quick brown fox should've jumped",
+#     'This is the second sentence with foxes apples'
+# ]
+
+st.header('About')
+"""
+This web app was developed by Silvia Hua, a data scientist and exercise physiologist.
+
+To learn more about Silvia, check out these links:
+* [LinkedIn](https://www.linkedin.com/in/silviahua/)
+* [Github](https://github.com/silvhua)
+
+If you are interested in estimating maximum (1RM) strength for the squat, check out her other project:
+* [5-minute project presentation](https://youtu.be/D7LPYTePY6k)
+* [1RM calculator using load-velocity profile](https://silvhua-lighthouse-capstone-project-srcapp-wafhso.streamlit.app/)
+* [Github repo](https://github.com/silvhua/lighthouse-capstone-project)
+"""
