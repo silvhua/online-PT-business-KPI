7663127123ef809bc709047fc7b9e7a8d9df2ac9
silvhua <silvhua@gmail.com>
2023-02-24 19:03:38 -0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,1 +1,12 @@
-print('hello world')
+import streamlit as st
+from processing import *
+
+st.write('hello world')
+
+docs = [   
+    'the quick brown fox',
+    'this is the second sentence with fox'
+]
+
+processed_docs = [preprocess_post_text(doc) for doc in docs]
+st.write(processed_docs)
