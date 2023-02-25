b3a985ffe70b35dc379b2bbd67e18ef68fe55018
silvhua <silvhua@gmail.com>
2023-02-24 20:32:26 -0800

    Update test.py

--- src/pages/test.py
+++ src/pages/test.py
@@ -14,5 +14,6 @@ processed_docs = [preprocess_post_text(doc) for doc in docs]
 # st.write(processed_docs)
 
 response_df = pd.read_csv('data/interim/my_ig_posts_2022_pulled_2023-02-11.csv') 
+st.write(response_df.head())
 
 # response_df = pd.read_csv('data/my_ig_posts_2022_pulled_2023-02-11.csv')  
