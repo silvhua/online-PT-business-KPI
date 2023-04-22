53732f76033297b2ebf8caa494157f541f6041bb
silvhua <silvhua@gmail.com>
2023-04-21 22:16:06 -0700

    extend access token; update_ig_account_insights

--- src/1_Instagram_Insights.py
+++ src/1_Instagram_Insights.py
@@ -30,7 +30,7 @@ ig_user_id_radio_input = st.radio('Select the Instagram account to query', ('sil
 
 if ig_user_id_radio_input != 'Other':
     try: # if running from local machine
-        with open("..\\notebooks\credentials.json") as f:
+        with open("..\\notebooks\credentials_long_lived.json") as f:
             credentials = json.load(f)
         ig_user_id_sh = credentials['ig_user_id']
         access_token_sh = credentials['access_token']
@@ -38,6 +38,7 @@ if ig_user_id_radio_input != 'Other':
         access_token_am = credentials['am_ig_access_token']
         ig_user_id_mf = credentials['mf_ig_user_id']
         access_token_mf = credentials['mf_access_token']
+        streamlit = False # Need this for subsequent script
     except: # if running from streamlit
         ig_user_id_sh = st.secrets['ig_user_id']
         access_token_sh = st.secrets['access_token']
@@ -45,18 +46,27 @@ if ig_user_id_radio_input != 'Other':
         access_token_am = st.secrets['am_ig_access_token']
         ig_user_id_mf = st.secrets['mf_ig_user_id']
         access_token_mf = st.secrets['mf_access_token']
+        streamlit = True
     if ig_user_id_radio_input == 'coach_mcloone':
         ig_user_id = ig_user_id_am
         access_token = access_token_am 
         timezone = 'Australia/Sydney'
+        access_token_key = 'am_ig_access_token'
     elif ig_user_id_radio_input == 'monikafronc_pilatesportal':
         ig_user_id = ig_user_id_mf
         access_token = access_token_mf 
         timezone = 'Canada/Pacific'
+        access_token_key = 'mf_access_token'
     else:
         ig_user_id = ig_user_id_sh
         access_token = access_token_sh
         timezone = 'Canada/Pacific'
+        access_token_key = 'access_token'
+        
+    prolong_access_token(
+        credentials_json='..\\notebooks\credentials_long_lived.json', 
+        access_token_key=access_token_key, 
+        new_credentials_filename='..\\notebooks\credentials_long_lived.json')
 else:
     
     """Note: This will only work for business Instagram accounts of 
@@ -115,8 +125,19 @@ if access_token != "":
         
         agg = 'sum'
 
-        df, insights_response_json = get_ig_account_insights(ig_user_id, access_token, 
-            since=posts_start_date, until=posts_end_date)
+        if streamlit:
+            json_path=r'data/API_response'
+            csv_path=r'data/interim'
+        else:
+            json_path=r'C:\Users\silvh\OneDrive\lighthouse\portfolio-projects\online-PT-social-media-NLP\data\API_response'
+            csv_path=r'C:\Users\silvh\OneDrive\lighthouse\portfolio-projects\online-PT-social-media-NLP\data\interim'
+        print('\njson_path:',json_path)
+        print(f'csv_path: {csv_path}')
+        print(f'IG user id: {ig_user_id_radio_input}\n')
+        df, insights_response_json = update_ig_account_insights(ig_user_id, access_token, 
+            since=posts_start_date, until=posts_end_date, filename=ig_user_id_radio_input,
+            json_path=json_path, csv_path=csv_path
+            )
 
         insights_plot = plot_account_insights(
             df, timezone=timezone, agg=agg, streamlit=True,
